dbd728f01f53f2b6aacf300664d854f5d5c6e1e6
Wyatt Baldwin <self@wyattbaldwin.com>
2021-06-26 05:32:48 -0700

    In arrivals view, ensure all stops exist before querying TriMet API

--- src/api/mystops/apps/arrivals/views.py
+++ src/api/mystops/apps/arrivals/views.py
@@ -6,6 +6,7 @@ from django.http import (
     JsonResponse,
 )
 
+from mystops.apps.stops.models import Stop
 from mystops.trimet import api
 
 
@@ -23,6 +24,7 @@ def arrivals(request):
             "Missing Query Parameter",
             "The q query parameter is required",
         )
+
     q = params["q"]
     if not q.strip():
         return make_error_response(
@@ -30,7 +32,9 @@ def arrivals(request):
             "Missing Query Parameter",
             "The q query parameter is required",
         )
+
     stop_ids = q.split(",")
+    stop_id_set = set()
     for i, stop_id in enumerate(stop_ids):
         try:
             stop_id = int(stop_id)
@@ -40,25 +44,44 @@ def arrivals(request):
                 f'Bad Stop ID: "{stop_id}"',
                 "TriMet stop IDs should be numbers",
             )
-        stop_ids[i] = stop_id
+        stop_id_set.add(stop_id)
+
+    # Ensure stop IDs exist before querying TriMet API
+    q = Stop.objects.filter(stop_id__in=stop_id_set)
+    existing_stop_ids = q.values_list("stop_id", flat=True)
+    num_existing_stop_ids = len(existing_stop_ids)
+    num_passed_stop_ids = len(stop_id_set)
+    if num_existing_stop_ids != num_passed_stop_ids:
+        delta = num_passed_stop_ids - num_existing_stop_ids
+        ess, verb = ("", "does") if delta == 1 else ("s", "do")
+        not_found = sorted(stop_id_set - set(existing_stop_ids))
+        not_found = ", ".join(str(id) for id in not_found)
+        return make_error_response(
+            404,
+            "Stop Not Found",
+            f"Stop ID{ess} {verb} not exist: {not_found}",
+        )
+
     try:
         arrivals = api.get_arrivals(settings.TRIMET.api_key, stop_ids)
     except api.TriMetAPIStopIDNotFoundError as exc:
         return make_error_response(
             404,
             "Stop Not Found",
-            f"Stop ID {exc.stop_id} doesn't exist",
+            f"Stop ID {exc.stop_id} does not exist",
         )
     except api.TriMetAPIError as exc:
         return make_error_response(502, f"TriMet API Error", str(exc))
+
     if arrivals["count"] == 0:
         ess = "" if len(stop_ids) == 1 else "s"
-        stop_ids = ', '.join(str(id) for id in stop_ids)
+        stop_ids = ", ".join(str(id) for id in stop_ids)
         return make_error_response(
             404,
             "No Arrivals Found",
             f"No arrivals found for stop{ess}: {stop_ids}",
         )
+
     return JsonResponse(arrivals)
 
 
