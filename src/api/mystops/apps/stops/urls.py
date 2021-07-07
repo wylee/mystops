0d07ca222f313810b36aa9f7ae7d348be2b1b6a3
Wyatt Baldwin <self@wyattbaldwin.com>
2021-07-07 00:16:56 -0700

    Add back GeoJSON stops view
    On the client, stop icons are clipped at tile boundaries when using MVT
    (but only sometimes...?). I couldn't figure out how to configure
    OpenLayers so they wouldn't be clipped. Leaving the unused MVT view for
    now in case there's a way to fix this.
    
    Refs f565a31b435e3564a42f1c26d1e3611b2097c9cb

--- src/api/mystops/apps/stops/urls.py
+++ src/api/mystops/apps/stops/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="stops.index"),
+    path("geojson", views.geojson, name="stops.geojson"),
     path("mvt/<int:z>/<int:x>/<int:y>", views.mvt, name="stops.mvt"),
     path("<int:stop_id>", views.stop, name="stops.stop"),
 ]
--- src/api/mystops/apps/stops/views.py
+++ src/api/mystops/apps/stops/views.py
@@ -50,6 +50,83 @@ def stop(request, stop_id):
     )
 
 
+GEOJSON_STATEMENT = """\
+SELECT row_to_json(feature_collection) AS feature_collection FROM (
+  SELECT
+    'FeatureCollection' AS type,
+    (SELECT items FROM (
+      SELECT 'name' AS type,
+      (SELECT p FROM (SELECT 'EPSG:4326' AS name) AS p) AS properties
+    ) AS items) AS crs,
+    array_agg(features) AS features FROM (
+      SELECT
+        'Feature' AS type,
+        ST_AsGeoJSON(stop.location)::json AS geometry,
+        'stop.' || stop.stop_id::text AS id,
+        (SELECT p FROM (
+          SELECT
+            stop.stop_id AS id,
+            stop.name,
+            stop.direction,
+            string_agg(route.short_name, ', ') AS routes
+          ) AS p
+        ) AS properties
+      FROM
+        stop
+      JOIN
+        stop_route
+        ON stop_route.stop_id = stop.id
+      JOIN
+        route
+        ON stop_route.route_id = route.id 
+      WHERE
+        stop.location && {envelope}
+      GROUP BY
+        stop.id
+      {limit}
+    ) AS features
+) AS feature_collection;\
+"""
+
+
+def get_envelope(request):
+    """Get envelope for bounding box query parameter ``bbox``."""
+    bbox = request.GET.get("bbox")
+    if not bbox:
+        return HttpResponseBadRequest("bbox query parameter is required")
+    coords = bbox.split(",")
+    if len(coords) != 4:
+        return HttpResponseBadRequest(f"Bad bounding box: {bbox}")
+    try:
+        coords = tuple(float(c) for c in coords)
+    except ValueError:
+        return HttpResponseBadRequest(f"Bad bounding box: {bbox}")
+    minx, miny, maxx, maxy = coords
+    if abs(minx) > 180 or abs(miny) > 90 or abs(maxx) > 180 or abs(maxy) > 90:
+        return HttpResponseBadRequest(f"Bad bounding box: {bbox}")
+    envelope = f"ST_MakeEnvelope({','.join(str(c) for c in coords)}, 4326)"
+    return envelope
+
+
+@cache_page(CACHE_TIME)
+def geojson(request, *, statement=GEOJSON_STATEMENT):
+    envelope = get_envelope(request)
+    if isinstance(envelope, HttpResponse):
+        return envelope
+    limit = request.GET.get("limit")
+    limit = int(limit) if limit else ""
+    statement = statement.format(envelope=envelope, limit=limit)
+    with connection.cursor() as cursor:
+        cursor.execute(statement)
+        row = cursor.fetchone()
+        if row is None:
+            return JsonResponse(None)
+        data = row[0]
+        if data["features"] is None:
+            data["features"] = []
+        return JsonResponse(row[0], safe=False)
+
+
 MVT_STATEMENT = """\
 SELECT
   ST_AsMVT(q, 'stops')
